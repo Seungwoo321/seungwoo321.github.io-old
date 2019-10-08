e4867068fee02311d278b6e1cc7a4063c71950fd
swlee <swlee@mz.co.kr>
2019-10-08 16:27:35 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ rm -rf docs/.vuepress/dist/
 npm run build
 cp -r static/ docs/.vuepress/dist/
 cd docs/.vuepress/dist/
-echo 'blog.devstory.kr' > CNAME
+# echo 'blog.devstory.kr' > CNAME
 git init 
 git config --local user.name "Seungwoo Lee"
 git config --local user.email "seungwoo321@gmail.com"
