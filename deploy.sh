49a49c1f2ed92d2e1bce34f178027897e5d5b98f
Seungwoo321 <seungwoo321@gmail.com>
2021-08-16 15:10:30 +0900

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-DATE=`date`
 set -e
 
 rm -rf docs/.vuepress/dist/ 
@@ -12,7 +11,7 @@ git init
 git config --local user.name "Seungwoo Lee"
 git config --local user.email "seungwoo321@gmail.com"
 git add -A
-git commit -m "deploy with vuepress script $DATE"
+git commit -m "deploy with vuepress script"
 git push -f git@github.com:Seungwoo321/Seungwoo321.github.io.git master:gh-pages
 
 cd -
